4fe421c39d93eed71b2e10089cbd358d34da08d2
Vaughn Cedric L. Araneta <vaughncedriclipardoaraneta@gmail.com>
2025-09-04 16:25:35 +0800

    unit tested the 'qlogicae_cli setup generic documentation' command

--- qlogicae_cli_test/qlogicae/kmand/tests/setup.ps1
+++ qlogicae_cli_test/qlogicae/kmand/tests/setup.ps1
@@ -25,6 +25,74 @@ Describe "[qlogicae_cli setup] test suite" {
         }
     }
 
+    Context ("[qlogicae_cli setup generic] test cases") {
+        It "[qlogicae_cli setup generic]: should terminate" {
+            $TestResult = qlogicae_cli setup generic | Out-String
+            
+            $TestResult | Should -BeNullOrEmpty
+        }
+
+        It "[qlogicae_cli setup generic --help]: should not be null or empty" {
+            $TestResult = qlogicae_cli setup vs2022 --help | Out-String
+            $QLogicaeKmandInstance.ConsoleLog($TestResult)
+
+            $TestResult | Should -Not -BeNullOrEmpty
+        }
+    }
+
+    Context ("[qlogicae_cli setup generic documentation] test cases") {
+        It "[qlogicae_cli setup generic documentation --help]: should not be null or empty" {
+            $TestResult = qlogicae_cli setup generic documentation --help | Out-String
+            $QLogicaeKmandInstance.ConsoleLog($TestResult)
+
+            $TestResult | Should -Not -BeNullOrEmpty
+        }
+
+        It "[qlogicae_cli setup generic documentation]: should not leave an empty folder" {
+            $TestResult = qlogicae_cli setup generic documentation | Out-String
+            $QLogicaeKmandInstance.ConsoleLog($TestResult)
+
+            $TestResult | Should -Not -BeNullOrEmpty
+            $QLogicaeKmandInstance.IsFolderEmpty("$($QLogicaeKmandInstance.Configurations.DotQLogicaeKmandEnvironmentFolderPath)") | Should -Be $false
+        }
+    }
+
+    Context ("[qlogicae_cli setup generic documentation --is-verbose-logging-enabled] test cases") {        
+        It "[qlogicae_cli setup generic documentation --is-verbose-logging-enabled='']: should not leave an empty folder" {
+            $TestResult = qlogicae_cli setup generic documentation --is-verbose-logging-enabled='' | Out-String
+            
+            $TestResult | Should -BeNullOrEmpty
+        }
+
+        It "[qlogicae_cli setup generic documentation --is-verbose-logging-enabled='fals']: should not leave an empty folder" {
+            $TestResult = qlogicae_cli setup generic documentation --is-verbose-logging-enabled='fals' | Out-String
+            
+            $TestResult | Should -BeNullOrEmpty
+        }
+
+        It "[qlogicae_cli setup generic documentation --is-verbose-logging-enabled='tru']: should not leave an empty folder" {
+            $TestResult = qlogicae_cli setup generic documentation --is-verbose-logging-enabled='tru' | Out-String
+            
+            $TestResult | Should -BeNullOrEmpty
+        }
+
+        It "[qlogicae_cli setup generic documentation --is-verbose-logging-enabled='false']: should not leave an empty folder" {
+            $TestResult = qlogicae_cli setup generic documentation --is-verbose-logging-enabled='false' | Out-String
+            $QLogicaeKmandInstance.ConsoleLog($TestResult)
+
+            $TestResult | Should -Not -BeNullOrEmpty
+            $QLogicaeKmandInstance.IsFolderEmpty("$($QLogicaeKmandInstance.Configurations.DotQLogicaeKmandEnvironmentFolderPath)") | Should -Be $false
+        }
+
+        It "[qlogicae_cli setup vs2022 application --is-verbose-logging-enabled='true']: should not leave an empty folder" {
+            $TestResult = qlogicae_cli setup vs2022 application --is-verbose-logging-enabled='true' | Out-String
+            $QLogicaeKmandInstance.ConsoleLog($TestResult)
+
+            $TestResult | Should -Not -BeNullOrEmpty
+            $QLogicaeKmandInstance.IsFolderEmpty("$($QLogicaeKmandInstance.Configurations.DotQLogicaeKmandEnvironmentFolderPath)") | Should -Be $false
+        }
+    }
+
     Context ("[qlogicae_cli setup vs2022] test cases") {
         It "[qlogicae_cli setup vs2022]: should terminate" {
             $TestResult = qlogicae_cli setup vs2022 | Out-String
